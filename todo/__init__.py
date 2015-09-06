b18a9bce7185231fdb1138a3f02bf9905d5f4019
wasw100 <wasw100@gmail.com>
2015-09-06 08:14:54 +0800

    使用render_template渲染首页

--- todo/__init__.py
+++ todo/__init__.py
@@ -1,9 +1,9 @@
 # -*- coding: utf-8 -*-
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 
 @app.route('/')
 def index():
-    return 'index'
+    return render_template('index.html')
